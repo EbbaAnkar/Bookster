cefb22bc5ec416b04d197de516f55a853a1e0863
arasvall <113009608+arasvall@users.noreply.github.com>
2023-06-02 13:55:29 +0200

    Update start.test.jsx

--- Bookster.v1/client/src/pages/start.test.jsx
+++ Bookster.v1/client/src/pages/start.test.jsx
@@ -5,16 +5,9 @@ import { fireEvent, render, screen } from '@testing-library/react'
 it('button clicked', () => {
     render(<App/>)
     
-   
     const button = screen.getByTestId('login')
-    // const button = screen.getByRole('button')
-    // const button = screen.getByText('Sign out')
-
+  
     fireEvent.click(button)
 
     expect(screen.getByTestId('login-form')).toBeDefined()
-
-    // fireEvent.click(button)
-
-    // expect(screen.queryByText('My logout')).toBeNull()
 })
